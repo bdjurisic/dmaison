2aa93acfaa0fba8f37153be9af14035946c74488
Balsa Djurisic <Balsa.Djurisic@coxautoinc.com>
2025-08-28 20:30:45 +0200

    DE and files Lustica

--- js/translate.js
+++ js/translate.js
@@ -7,15 +7,23 @@
         contactUsHeading: `Contact`,
         contactUsDescription: `Looking for exquisite <b>luxury</b> properties or top-tier <b>real estate</b> services in Montenegro? <br><br> Whether you're <b>buying</b>, <b>selling</b>, or <b>renting</b> villas, hotels, or unique residences, we’ve got you covered. Our dedicated experts ensure seamless transactions and tailored solutions to match your specific needs. <b>Contact us today!</b>`,
         location: `Podgorica, Montenegro`,
+        lb_offer_title: "Luštica Bay",
+        lb_offer_description: "We are offering exclusive sales in Luštica Bay. Discover all the details in the provided document and feel free to contact us for further information or inquiries.",
+        lb_offer_label: "See more information",
+        lb_offer_pdf_link: "https://lusticabay.com/media/brochures/Quick_Links_Brokers_Package_ENG.pdf"
     },
     fr: {
-        tagline: `ouvrez les portes d’un <i class="gold-color">art de vivre unique</i>. Des <i class="gold-color">villas côtières</i> baignées de soleil aux <i class="gold-color">appartements urbains</i> élégants, chaque résidence est une œuvre d’exception. Découvrez l’<i class="gold-color">élégance suprême</i> du Monténégro <i class="gold-color">— un privilège pour ceux qui vivent l’exception</i>.`,
+        tagline: `ouvre les portes d’un <i class="gold-color">art de vivre unique</i>. Des <i class="gold-color">villas côtières</i> baignées de soleil aux <i class="gold-color">appartements urbains</i> élégants, chaque résidence est une œuvre d’exception. Découvrez l’<i class="gold-color">élégance suprême</i> du Monténégro <i class="gold-color">— un privilège pour ceux qui vivent le prestige</i>.`,
         feature1: `Trouvez votre <b>villa de luxe de rêve</b> ou hôtel au Monténégro, conçu pour dépasser vos attentes en matière de style de vie.`,
         feature2: `Vendez votre propriété en toute simplicité et confiance, grâce à nos <span class="highlight">professionnels experts de l'immobilier</span>.`,
         feature3: `Louez des <i class="gold-color">maisons</i> magnifiques ou des espaces exclusifs adaptés à vos besoins en matière de voyages ou de vie de luxe.`,
         contactUsHeading: `Contact`,
         contactUsDescription: `Vous recherchez des propriétés <b>luxueuses</b> ou des services <b>immobiliers haut de gamme</b> au Monténégro? <br><br> Que vous soyez en train d'<b>acheter</b>, de <b>vendre</b> ou de <b>louer</b> des villas, hôtels ou résidences uniques, nous sommes là pour vous. Nos experts dédiés garantissent des transactions fluides et des solutions sur mesure pour répondre à vos besoins spécifiques. <b>Contactez-nous dès aujourd'hui !</b>`,
         location: `Podgorica, Monténégro`,
+        lb_offer_title: "Luštica Bay",
+        lb_offer_description: "Nous proposons des ventes exclusives à Luštica Bay. Découvrez tous les détails dans le document fourni et n'hésitez pas à nous contacter pour plus d'informations ou toute question.",
+        lb_offer_label: "Télécharger les informations de vente",
+        lb_offer_pdf_link: "https://lusticabay.com/media/brochures/Quick_Links_Brokers_Package_ENG.pdf"
     },
     ru: {
         tagline: `oткрывает двери в <i class="gold-color">дом вашей мечты</i>. От солнечных прибрежных <i class="gold-color">вилл</i> до современных городских <i class="gold-color">квартир</i> — каждая недвижимость произведение искусства. Откройте для себя высший уровень жизни в Черногории <br> <i class="gold-color"> — для тех, кто живет за пределами обыденного</i>.`,
@@ -25,6 +33,23 @@
         contactUsHeading: `Контакты`,
         contactUsDescription: `Ищете элитные <b>недвижимости</b> или высококачественные услуги в Черногории? <br><br> Покупаете, <b>продаете</b> или <b>сдаете</b> виллы, отели или уникальные резиденции — мы к вашим услугам. Наши специалисты обеспечат легкие транзакции. <b>Свяжитесь с нами!</b>`,
         location: `Подгорица, Черногория`,
+        lb_offer_title: "Луштица Бэй",
+        lb_offer_description: "Мы предлагаем эксклюзивные продажи в Луштица Бэй. Ознакомьтесь со всеми деталями в предоставленном документе и свяжитесь с нами для получения дополнительной информации или вопросов.",
+        lb_offer_label: "Скачать информацию о продаже",
+        lb_offer_pdf_link: "https://lusticabay.com/media/brochures/Quick_Links_Brokers_Package_RUS.pdf"
+    },
+    de: {
+        tagline: `öffnet die Türen zu Ihrem <i class="gold-color">Traumhaus</i>. Von sonnenverwöhnten Küsten-<i class="gold-color">villen</i> bis hin zu modernen Stadt-<i class="gold-color">apartments</i>, jede Immobilie ist ein Meisterwerk. Erleben Sie das Beste des Lebensstils in Montenegro <br> <i class="gold-color"> — für diejenigen, die über das Gewöhnliche hinaus leben</i>.`,
+        feature1: `Finden Sie Ihre <b>Traumvilla</b> oder Ihr Luxushotel in Montenegro, entworfen, um Ihre Lifestyle-Erwartungen zu übertreffen.`,
+        feature2: `Verkaufen Sie Ihre Immobilie mit Leichtigkeit und Vertrauen, betreut von unseren <span class="highlight">professionellen Immobilienexperten</span>.`,
+        feature3: `Mieten Sie atemberaubende <i class="gold-color">Häuser</i> oder exklusive Räume, die auf Ihre luxuriösen Reise- oder Wohnbedürfnisse zugeschnitten sind.`,
+        contactUsHeading: `Kontakt`,
+        contactUsDescription: `Suchen Sie exklusive <b>Luxusimmobilien</b> oder erstklassige <b>Immobiliendienstleistungen</b> in Montenegro? <br><br> Ob Sie <b>kaufen</b>, <b>verkaufen</b> oder <b>mieten</b> – Villen, Hotels oder einzigartige Residenzen – wir sind für Sie da. Unsere engagierten Experten sorgen für reibungslose Transaktionen und maßgeschneiderte Lösungen, die genau auf Ihre Bedürfnisse zugeschnitten sind. <b>Kontaktieren Sie uns noch heute!</b>`,
+        location: `Podgorica, Montenegro`,
+        lb_offer_title: "Luštica Bay",
+        lb_offer_description: "Wir bieten exklusive Verkaufsangebote in Luštica Bay an. Finden Sie alle Details im bereitgestellten Dokument und kontaktieren Sie uns gerne für weitere Informationen oder Fragen.",
+        lb_offer_label: "Verkaufsinformationen herunterladen",
+        lb_offer_pdf_link: "https://lusticabay.com/media/brochures/Quick_Links_Brokers_Package_GER.pdf"
     },
 };
 
@@ -45,6 +70,11 @@ function switchLanguage(lang) {
         document.getElementById("contactUsHeading").innerHTML = langData.contactUsHeading;
         document.getElementById("contactUsDescription").innerHTML = langData.contactUsDescription;
         document.getElementById("location").innerHTML = langData.location;
+        document.getElementById("offer-title").innerHTML = langData.lb_offer_title;
+        document.getElementById("offer-description").innerHTML = langData.lb_offer_description;
+        document.getElementById("offer-label").innerHTML = langData.lb_offer_label;
+        document.getElementById("offer-pdf-button").setAttribute('href', langData.lb_offer_pdf_link);
+        
     }
 }
 
